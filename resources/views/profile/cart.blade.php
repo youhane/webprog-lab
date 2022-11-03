3590022fb5e8f2b03e271b5980c795501dcd8ac3
youhane <jason.sebastian154@gmail.com>
2022-11-04 01:20:23 +0700

    add: history page a bit more styled

--- resources/views/profile/cart.blade.php
+++ resources/views/profile/cart.blade.php
@@ -1,63 +1,55 @@
 @extends('layouts.base')
 @section('content')
-    <section class="container py-5">
+    <section class="container px-lg-5">
         @if (isset($transaction))
-            <table class="table">
-                <tr>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                    <th>Subtotal</th>
-                </tr>
-                @foreach ($transaction->details as $product)
-                    <tr class="d-flex flex-column flex-lg-row">
-                        <td>
-                            <div>
-                                <a href="/products/{{ $product->product->slug }}">
-                                    <div>
-                                        @if ($product->product->image == null)
-                                            <img src="{{ asset('storage/product-images/no-image.png') }}" class="cart-img"
-                                                alt="...">
-                                        @else
-                                            @if (substr($product->product->image, 0, 4) == 'http')
-                                                <img src="{{ $product->product->image }}" class="cart-img">
-                                            @else
-                                                <img src="{{ asset('storage/product-images/' . $product->product->image) }}"
-                                                    class="cart-img" alt="...">
-                                            @endif
-                                        @endif
-                                    </div>
-                                </a>
+            <h1 class="py-3">Cart</h1>
+            @foreach ($transaction->details as $product)
+                <div class="card w-75 mx-auto my-3">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-evenly flex-lg-row gap-5">
+                        <div>
+                            <a href="/products/{{ $product->product->slug }}">
                                 <div>
-                                    <p>{{ $product->product->name }}</p>
-                                    <small>Price: Rp. {{ number_format($product->product->price, 2) }}</small>
-                                    <br />
-                                    <form action="/cart/delete" method="POST">
-                                        @csrf
-                                        @method('put')
-                                        <input type="hidden" name="product_id" value="{{ $product->product->id }}">
-                                        <input type="hidden" name="transaction_id" value="{{ $transaction->id }}">
-                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                    </form>
+                                    @if ($product->product->image == null)
+                                        <img src="{{ asset('storage/product-images/no-image.png') }}" class="cart-img"
+                                            alt="...">
+                                    @else
+                                        @if (substr($product->product->image, 0, 4) == 'http')
+                                            <img src="{{ $product->product->image }}" class="cart-img">
+                                        @else
+                                            <img src="{{ asset('storage/product-images/' . $product->product->image) }}"
+                                                class="cart-img" alt="...">
+                                        @endif
+                                    @endif
                                 </div>
-                            </div>
-                        </td>
-                        <td>
+                            </a>
+                        </div>
+                        <div>
+                            <h3>{{ $product->product->name }}</h3>
                             <form action="/cart/quantity" method="POST">
                                 @csrf
                                 @method('put')
                                 <label for="quantity">Quantity</label>
-                                <div class="mb-3">
+                                <div class="input-group mb-3 w-75 fs-6">
                                     <input type="hidden" name="product_id" value="{{ $product->product->id }}">
                                     <input type="hidden" name="transaction_id" value="{{ $transaction->id }}">
-                                    <input type="number" name="quantity" value="{{ $product->quantity }}">
+                                    <input type="number" class="form-control" name="quantity"
+                                        value="{{ $product->quantity }}">
                                     <button type="submit" class="btn btn-primary">Update</button>
                                 </div>
                             </form>
-                        </td>
-                        <td>Rp. {{ number_format($product->product->price * $product->quantity, 2) }}</td>
-                    </tr>
-                @endforeach
-            </table>
+                            <p>Price: Rp. {{ number_format($product->product->price, 2) }}</p>
+                            <p>Total: Rp. {{ number_format($product->product->price * $product->quantity, 2) }}</p>
+                            <form action="/cart/delete" method="POST">
+                                @csrf
+                                @method('put')
+                                <input type="hidden" name="product_id" value="{{ $product->product->id }}">
+                                <input type="hidden" name="transaction_id" value="{{ $transaction->id }}">
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
             <form action="/cart/pay" method="POST" class="p-4 bg-light border border-1 my-3 rounded">
                 @csrf
                 <input type="hidden" name="transaction_id" value="{{ $transaction->id }}">
--- resources/views/profile/history.blade.php
+++ resources/views/profile/history.blade.php
@@ -1,35 +1,50 @@
 @extends('layouts.base')
 @section('content')
     <div class="container">
-        <div class="row">
-            @if (count($transactions))
-                <h1>History</h1>
-                @foreach ($transactions as $transaction)
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <h1>{{ $transaction->total_price }}</h1>
-                                    <h1>{{ $transaction->updated_at->toDayDateTimeString() }}</h1>
-                                </div>
-                                <div class="col-md-6">
-                                    @foreach ($transaction->details as $item)
+        @if (count($transactions))
+            <h1 class="py-5">History</h1>
+            @foreach ($transactions as $transaction)
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Transaction No. {{ $transaction->id }}</h1>
+                        <h3>{{ $transaction->updated_at->toDayDateTimeString() }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            @foreach ($transaction->details as $item)
+                                <div class="d-flex">
+                                    <div>
+                                        @if ($item->product->image == null)
+                                            <img src="{{ asset('storage/product-images/no-image.png') }}" class="cart-img"
+                                                alt="...">
+                                        @else
+                                            @if (substr($item->product->image, 0, 4) == 'http')
+                                                <img src="{{ $item->product->image }}" class="cart-img">
+                                            @else
+                                                <img src="{{ asset('storage/product-images/' . $item->product->image) }}"
+                                                    class="cart-img" alt="...">
+                                            @endif
+                                        @endif
+                                    </div>
+                                    <div class="d-flex flex-column">
                                         <h3>{{ $item->product->name }}</h3>
                                         <p>Price: Rp. {{ number_format($item->product->price, 2) }}</p>
                                         <p>Quantity: {{ $item->quantity }}</p>
                                         <p>Total: Rp. {{ number_format($item->product->price * $item->quantity, 2) }}</p>
-                                    @endforeach
+                                    </div>
                                 </div>
-                            </div>
+                            @endforeach
                         </div>
+                        <h1>Total Rp. {{ number_format($transaction->total_price, 2) }}</h1>
                     </div>
-                @endforeach
-            @else
-                <section class="container text-center d-flex align-items-center justify-content-center position-relative">
-                    <h1 class="center-content">Looks like you don't have any transactions yet, let me get you back <a
-                            href="/">here</a></h1>
-                </section>
-            @endif
-        </div>
+                </div>
+    </div>
+    @endforeach
+@else
+    <section class="container text-center d-flex align-items-center justify-content-center position-relative">
+        <h1 class="center-content">Looks like you don't have any transactions yet, let me get you back <a
+                href="/">here</a></h1>
+    </section>
+    @endif
     </div>
 @endsection
